93b690bbeba836a36796ed480ccab8ac614f4226
NeLLo <officialnellorecords@gmail.com>
2021-03-16 22:27:04 +0100

    fixed icon

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <meta name="twitter:image:alt" content="https://nellorecords.com/images/WhiteNeLLoLogo.jpg">
     <link rel="icon"
           type="image/jpg"
-          href="http://www.nellorecords.com/NeLLoIcon.jpg">
+          href="http://www.nellorecords.com/images/favicon.ico">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
     </style>
